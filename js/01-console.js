a44147dc921118a82b21bd2077b1aec10cea96b4
Rimantas <belauzas@yahoo.com>
2023-01-16 10:15:12 +0200

    Numbers

--- js/01-console.js
+++ js/01-console.js
@@ -12,3 +12,12 @@ console.log(5);
 console.log(3, 14);
 console.log(3.14);
 console.log(1, 2, 3, 4, 5, 6);
+
+/*
+Esmines busimos temos:
+- kintamieji (atsiminti informacija, duomenys)
+- palyginimas
+- ciklai (proceduru kartojimas)
+- funkcijos
+- event listeners (narsykleje)
+*/
